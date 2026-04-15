7814c8e124a92499988dd592f9fe2d684d6b760a
piparakanika <piparakanika@gmail.com>
2026-04-15 20:56:30 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,7 +13,7 @@ locals {
   region          = "ap-southeast-1"
   name            = "qualibytes-eks-cluster"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = ["ap-south-1a", "ap-south-1b"]
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
